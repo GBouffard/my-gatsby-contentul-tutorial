dd60246fb00dd2be6a867c3bc3c0b2f0f07ac8f9
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-09-16 15:33:30 +0100

    Apply a full style on a page and link to props with classnames (CSS modules)

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,7 +1,19 @@
 import React, { Fragment } from "react"
 import { Link } from "gatsby"
 
+import styles from "./about-css-modules.module.css"
 import Container from "../components/css-container"
+
+// props and styles have the same names so we can find them easily
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default () => (
   <Fragment>
@@ -9,6 +21,18 @@ export default () => (
       <h1>A page about CSS Modules</h1>
       <p>CSS Modules are cool</p>
     </Container>
+
+    <User
+      username="Guillaume Bouffard"
+      avatar="https://avatars1.githubusercontent.com/u/10553003?s=460&v=4"
+      excerpt="I'm Guillaume Bouffard. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="GB Evil's twin"
+      avatar="https://avatars1.githubusercontent.com/u/10553003?s=460&v=4"
+      excerpt="I'm GB Evil's twin, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+
     <Link to="/">Back to home page</Link>
   </Fragment>
 )
